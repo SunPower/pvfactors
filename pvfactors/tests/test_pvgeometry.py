17d682f8ccad6081d0d0fe108081c987e954b655
anomam <marc.abouanoma@sunpowercorp.com>
2018-07-30 17:18:28 +0900

    Update pandas version and test centroids for registry

--- pvfactors/tests/test_pvgeometry.py
+++ pvfactors/tests/test_pvgeometry.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+"""
+Test the implementation of the pvgeometry helper functions
+"""
+
 import numpy as np
 import pandas as pd
 from shapely.geometry import LineString, Point
@@ -16,5 +20,18 @@ def test_point_boundaries():
     linestring = LineString(list_points)
     registry = pd.DataFrame({'geometry': [linestring]})
 
+    # ``boundary`` returns ``MultiPoint`` objects, and property ``geoms``
+    # allows to get the actual points
     assert registry.pvgeometry.boundary[0].geoms[0] == list_points[0]
     assert registry.pvgeometry.boundary[0].geoms[1] == list_points[1]
+
+
+def test_centroids():
+    """ Testing that the centroid values are correctly calculated """
+
+    list_linestrings = [LineString([Point(0, 0), Point(3, 2)]),
+                        LineString([Point(0, 1), Point(1, 0)])]
+    registry = pd.DataFrame({'geometry': list_linestrings})
+
+    assert registry.pvgeometry.centroid.values[0] == Point(1.5, 1)
+    assert registry.pvgeometry.centroid.values[1] == Point(0.5, 0.5)
