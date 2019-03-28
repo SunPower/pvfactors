9cb24aa61a327a50cb3ea4b6334aac1c4aa63e4a
anomam <marc.abouanoma@sunpowercorp.com>
2019-03-28 16:52:11 +0900

    Updated calculation of surface normal vector

--- pvfactors/pvsurface.py
+++ pvfactors/pvsurface.py
@@ -1,3 +1,5 @@
+import numpy as np
+from pvfactors.config import DEFAULT_NORMAL_VEC
 from shapely.geometry import LineString, GeometryCollection
 
 
@@ -6,10 +8,29 @@ class BaseSurface(LineString):
     but adding an orientation to it. So two surfaces could use the same
     linestring, but have opposite orientations."""
 
-    def __init__(self, coords, normal_vector):
+    def __init__(self, coords, normal_vector=None):
+        """Normal vector can have two directions for a given LineString,
+        so the user can provide it in order to be specific,
+        otherwise it will be automatically
+        calculated, but then the surface won't know if it was supposed to be
+        pointing "up" or "down". If the surface is empty, the normal vector
+        will take the default value."""
 
         super(BaseSurface, self).__init__(coords)
-        self.n_vec = normal_vector
+        if normal_vector is None:
+            self.n_vector = self._calculate_n_vector()
+        else:
+            self.n_vector = normal_vector
+
+    def _calculate_n_vector(self):
+        """Calculate normal vector of the surface if not empty"""
+        if not self.is_empty:
+            b1, b2 = self.boundary
+            dx = b2.x - b1.x
+            dy = b2.y - b1.y
+            return np.array([-dy, dx])
+        else:
+            return DEFAULT_NORMAL_VEC
 
 
 class PVSurface(BaseSurface):
@@ -36,8 +57,9 @@ class PVSurface(BaseSurface):
 
 
 class PVSegment(GeometryCollection):
-    """A PV segment will be a collection of 2 PV surfaces,
-    a shaded one and an illuminated one. It inherits from ``shapely``
+    """A PV segment will be a collection of 2 collinear and contiguous
+    PV surfaces, a shaded one and an illuminated one. It inherits from
+    ``shapely``
     :py:class:`GeometryCollection` so that we can still call basic geometrical
     methods and properties on it, eg call length and get sum of lengths
     here of the two surfaces"""
@@ -50,6 +72,18 @@ class PVSegment(GeometryCollection):
         self._illum_surface = illum_surface
         super(PVSegment, self).__init__([self._shaded_surface,
                                          self._illum_surface])
+
+    @property
+    def n_vector(self):
+        """Since shaded and illum surfaces are supposed to be collinear,
+        this should return either surfaces' normal vector. If both empty,
+        return None."""
+        if not self.illum_surface.is_empty:
+            return self.illum_surface.n_vector
+        elif not self.shaded_surface.is_empty:
+            return self.shaded_surface.n_vector
+        else:
+            return DEFAULT_NORMAL_VEC
 
     @property
     def shaded_surface(self):
