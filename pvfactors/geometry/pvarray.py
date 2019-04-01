dec01f9d4e8d800854a31429982b8d90ea36a1ea
anomam <marc.abouanoma@sunpowercorp.com>
2019-04-01 12:37:28 +0900

    Adding other array parameters

--- pvfactors/geometry/pvarray.py
+++ pvfactors/geometry/pvarray.py
@@ -10,9 +10,16 @@ class OrderedPVArray(object):
     and also all equally spaced. These simplifications allow faster and easier
     calculations."""
 
-    def __init__(self, list_pvrows=[], ground=None):
+    def __init__(self, list_pvrows=[], ground=None, surface_tilt=None,
+                 surface_azimuth=None, solar_zenith=None, solar_azimuth=None,
+                 gcr=None):
         self.pvrows = list_pvrows
         self.ground = ground
+        self.gcr = gcr
+        self.solar_zenith = solar_zenith
+        self.solar_azimuth = solar_azimuth
+        self.surface_tilt = surface_tilt
+        self.surface_azimuth = surface_azimuth
 
     @classmethod
     def from_dict(cls, parameters):
@@ -22,14 +29,19 @@ class OrderedPVArray(object):
         list_pvrows = []
         width = parameters['pvrow_width']
         tilt = parameters['surface_tilt']
+        gcr = parameters['gcr']
         y_center = parameters['pvrow_height']
-        distance = width / parameters['gcr']
+        distance = width / gcr
         for idx in range(parameters['n_pvrows']):
             x_center = X_ORIGIN_PVROWS + idx * distance
             pvrow = PVRow.from_center_tilt_width(
                 (x_center, y_center), tilt, width, index=idx)
             list_pvrows.append(pvrow)
-        return cls(list_pvrows=list_pvrows, ground=ground)
+        return cls(list_pvrows=list_pvrows, ground=ground, surface_tilt=tilt,
+                   surface_azimuth=parameters['surface_azimuth'],
+                   solar_zenith=parameters['solar_zenith'],
+                   solar_azimuth=parameters['solar_azimuth'],
+                   gcr=gcr)
 
     def update_tilt(self, new_tilt):
         pass
