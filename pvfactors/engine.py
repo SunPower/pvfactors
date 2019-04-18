8ad35fee1ba1e8c21c46291df0f45a25840a9c6a
anomam <marc.abouanoma@sunpowercorp.com>
2019-04-18 18:04:38 +0900

    Updated engine docstrings

--- pvfactors/engine.py
+++ pvfactors/engine.py
@@ -1,4 +1,6 @@
-"""Engine that will run complete pvfactors calculations"""
+"""This module contains the engine classes that will run the complete
+timeseries simulations."""
+
 import numpy as np
 from pvfactors.geometry import OrderedPVArray
 from pvfactors.viewfactors import VFCalculator
@@ -15,7 +17,27 @@ class PVEngine(object):
     def __init__(self, params, vf_calculator=VFCalculator(),
                  irradiance_model=HybridPerezOrdered(),
                  cls_pvarray=OrderedPVArray):
-        """Create pv engine class, and initialize timeseries parameters"""
+        """Create pv engine class, and initialize timeseries parameters.
+
+        Parameters
+        ----------
+        params : dict
+            The parameters defining the PV array
+        vf_calculator : vf calculator object, optional
+            Calculator that will be used to calculate the view factor matrices
+            (Default =
+            :py:class:`~pvfactors.viewfactors.calculator.VFCalculator` object)
+        irradiance_model : irradiance model object, optional
+            The irradiance model that will be applied to the PV array
+            (Default =
+            :py:class:`~pvfactors.irradiance.models.HybridPerezOrdered`
+            object)
+        cls_pvarray : class of PV array, optional
+            Class that will be used to build the PV array
+            (Default =
+            :py:class:`~pvfactors.geometry.pvarray.OrderedPVArray` class)
+
+        """
         self.params = params
         self.vf_calculator = vf_calculator
         self.irradiance = irradiance_model
@@ -30,7 +52,32 @@ class PVEngine(object):
 
     def fit(self, timestamps, DNI, DHI, solar_zenith, solar_azimuth,
             surface_tilt, surface_azimuth, albedo):
-        """Save timeseries angle data and fit the irradiance model"""
+        """Fit the timeseries data to the engine. More specifically,
+        save all the parameters that needs to be saved, and perform the
+        irradiance transformations required by the irradiance model.
+        Note that all angles follow the pvlib-python angle convention: North -
+        0 deg, East - 90 deg, etc.
+
+        Parameters
+        ----------
+        timestamps : array-like
+            List of timestamps of the simulation.
+        DNI : array-like
+            Direct normal irradiance values [W/m2]
+        DHI : array-like
+            Diffuse horizontal irradiance values [W/m2]
+        solar_zenith : array-like
+            Solar zenith angles [deg]
+        solar_azimuth : array-like
+            Solar azimuth angles [deg]
+        surface_tilt : array-like
+            Surface tilt angles, from 0 to 180 [deg]
+        surface_azimuth : array-like
+            Surface azimuth angles [deg]
+        albedo : array-like
+            Albedo values (or ground reflectivity)
+
+        """
         # Save
         if np.isscalar(DNI):
             timestamps = [timestamps]
@@ -57,7 +104,20 @@ class PVEngine(object):
                             self.params['rho_back_pvrow'], albedo)
 
     def run_timestep(self, idx):
-        """Run timestep"""
+        """Run simulation for a single timestep index.
+
+        Parameters
+        ----------
+        idx : int
+            Index for which to run simulation
+
+        Returns
+        -------
+        pvarray : PV array object
+            PV array object after all the calculations are performed and
+            applied to it
+
+        """
 
         # Check that the sun is up, if not do not run calculations
         solar_zenith = self.solar_zenith[idx]
@@ -108,7 +168,22 @@ class PVEngine(object):
         return pvarray
 
     def run_all_timesteps(self, fn_build_report=None):
-        """Run all simulation timesteps and return report"""
+        """Run all simulation timesteps and return a report that will be built
+        by the function passed by the user.
+
+        Parameters
+        ----------
+        fn_build_report : function, optional
+            Function that will build the report of the simulation
+            (Default value = None)
+
+        Returns
+        -------
+        report
+            Saved results from the simulation, as specified by user's report
+            function
+
+        """
 
         report = None
         for idx in tqdm(range(self.n_points)):
