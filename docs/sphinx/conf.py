a5f4e58ecff31f4e88caa8218c7992487e6aa246
anomam <marc.abouanoma@sunpowercorp.com>
2019-06-04 18:51:44 +0900

    Use small logo in docs

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -162,7 +162,7 @@ rst_prolog = """
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/logo.png'
+html_logo = '_static/logo_small.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
