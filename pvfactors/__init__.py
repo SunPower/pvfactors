a6088bd51c5fae43a97170f45a7de6761d445411
Kevin Anderson <kevin.anderson@nrel.gov>
2021-02-04 10:17:18 -0700

    add import check to __init__.py

--- pvfactors/__init__.py
+++ pvfactors/__init__.py
@@ -4,6 +4,22 @@ from pvfactors.version import __version__
 import logging
 logging.basicConfig()
 
+try:
+    from shapely.geos import lgeos
+except OSError as err:
+    # https://github.com/SunPower/pvfactors/issues/109
+    msg = (
+        "pvfactors encountered an error when importing the shapely package. "
+        "This often happens when a binary dependency is missing because "
+        "shapely was installed from PyPI using pip. Try reinstalling shapely "
+        "from another source like conda-forge with "
+        "`conda install -c conda-forge shapely`, or alternatively from "
+        "Cristoph Gohlke's website if you're on Windows: "
+        "https://www.lfd.uci.edu/~gohlke/pythonlibs/#shapely"
+    )
+    err.strerror += "; " + msg
+    raise err
+
 
 class PVFactorsError(Exception):
     pass
